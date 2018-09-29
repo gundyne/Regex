a36e408bd36b87daded5f1159b257663f7c1719e
gundyne <dathuydoan@gmail.com>
2018-09-28 17:08:12 -0700

    Comments to Regex File

--- Project1/src/regex/Regex.java
+++ Project1/src/regex/Regex.java
@@ -6,14 +6,10 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
- *
- * @author Dat
+ * @author Dat Doan
+ * @email dathuydoan@gmail.com
+ * This class is the main class of the project and runs first
  */
 public class Regex {
 
@@ -22,12 +18,14 @@ public class Regex {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Creating tokenizer to handle tokens found from the file
         Tokenizer tokTest = new Tokenizer(args[0]);
+        // Determine where the file being opened is stored at
         String fileLocation = args[0].substring(0, args[0].lastIndexOf("\\")+1);
         ArrayList<Token> tokenList = tokTest.printTokens();
         try {
             try (Writer fileWriter = new FileWriter(new File(fileLocation+"output.txt"),false)) {
+                //Writing tokens to output.txt
                 for(Token t: tokenList)
                     fileWriter.write(t.toString()+'\n');
             }
